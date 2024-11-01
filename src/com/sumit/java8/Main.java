73f8e9e710de59806ee9701e454d4ed603f87f2a
Sumit Mittal <sumit.mittal@ericsson.com>
2024-11-01 15:01:58 -0400

    code modified

--- src/com/sumit/java8/Main.java
+++ src/com/sumit/java8/Main.java
@@ -3,7 +3,7 @@ package com.sumit.java8;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test commit");
+        System.out.println("Test commit from intelliJ");
     }
 
 }
