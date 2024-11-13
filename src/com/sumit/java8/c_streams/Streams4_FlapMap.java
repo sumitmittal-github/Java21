401c099ca87b8b28fc2cbb8bbb33ea0b1165f16e
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-12 20:49:10 -0500

    Java 8 : Streams : Flat map with filter, sorted and distinct

---
+++ src/com/sumit/java8/c_streams/Streams4_FlapMap.java
@@ -0,0 +1,25 @@
+package com.sumit.java8.c_streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Streams4_FlapMap {
+
+    public static void main(String[] args) {
+        List<String> list1 = Arrays.asList("a", "b");
+        List<String> list2 = Arrays.asList("d", "c");
+        List<String> list3 = Arrays.asList("a", "c");
+
+        List<String> listOfStrings = Stream.of(list1, list2, list3)
+                .filter(list -> list.contains("c"))     // [d c]  [a c]
+                .flatMap(list -> list.stream())         // [d c a c]
+                .sorted()                               // [a c c d]
+                .distinct()                             // [a c d]
+                .collect(Collectors.toList());
+
+        System.out.println(listOfStrings);
+    }
+
+}
