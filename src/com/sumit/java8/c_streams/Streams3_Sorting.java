2d83311b5eb4a97532f575c579902454e1762401
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-12 20:28:06 -0500

    Java 8 : Stream : Sorting a list of custom objects with sorted()

--- src/com/sumit/java8/c_streams/Streams3_Sorting.java
+++ src/com/sumit/java8/c_streams/Streams3_Sorting.java
@@ -9,10 +9,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Streams3_Sorting {
 
     public static void main(String[] args) {
+
+        // sorting a list of string
+        List<String> names = Arrays.asList("Sumit", "Lokesh", "Ram");
+        names.stream().sorted().forEach(System.out::println);
+                                // or
+        List<String> names2 = names.stream().sorted().collect(Collectors.toList());
+        System.out.println(names2);
+
+
+        // sorting a list of custom object
         List<Person> list = Arrays.asList(
                 new Person("Sumit", 21, new Date()),
                 new Person("Lokesh", 23, new Date()),
