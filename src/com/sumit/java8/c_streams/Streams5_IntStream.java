9e68b1515ba7f0e67fa14a5dd4a866fa579fd351
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-13 07:39:51 -0500

    Java 8 : Stream : IntStream

---
+++ src/com/sumit/java8/c_streams/Streams5_IntStream.java
@@ -0,0 +1,20 @@
+package com.sumit.java8.c_streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+
+public class Streams5_IntStream {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+
+        // sum of 1,2,3 using IntStream
+        int sum = list.stream().mapToInt(i -> i.intValue()).sum();
+        System.out.println("Sum of 1, 2, 3 = " + sum);
+
+        OptionalInt max = list.stream().mapToInt(i -> i.intValue()).max();
+        System.out.println("Max of 1, 2, 3 = " + max.orElse(Integer.MIN_VALUE));
+    }
+
+}
