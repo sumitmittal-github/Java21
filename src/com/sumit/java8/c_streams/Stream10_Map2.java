6748888b9c123402110db044e0a2fc9df7eb684a
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-14 19:36:05 -0500

    Java8 : Stream : List to Map with collect(Collectors.toMap)

---
+++ src/com/sumit/java8/c_streams/Stream10_Map2.java
@@ -0,0 +1,43 @@
+package com.sumit.java8.c_streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+
+public class Stream10_Map2 {
+
+    // Assignment : List<Book> -> Map<String, Double> using collect()
+
+    public static void main(String[] args) {
+
+        List<Book> books = Arrays.asList(
+                new Book("Basics of Java programming", 10.0),
+                new Book("Basics of Java programming", 20.0),
+                new Book("A journey to Java 21", 30.0),
+                new Book("New features of Java 17", 20.0)
+        );
+        /*
+        books.stream()
+                .collect(Collectors.toMap(book -> book.getTitle(), book -> book.getPrice()))
+                .forEach( (k,v) -> System.out.println(k + " : " + v));
+        */
+
+        // above code gave below exception, because duplicate keys
+        // Exception in thread "main" java.lang.IllegalStateException: Duplicate key Basics of Java programming (attempted merging values 10.0 and 20.0)
+
+
+        // Solution : collect() method allows aus to pass 3rd parameters in case keys are duplicate
+        Map<String, Double> maps1 = books.stream()
+                                         .collect( Collectors.toMap(     book -> book.getTitle(), book -> book.getPrice(),     (v1, v2) -> v1+v2));
+        maps1.forEach( (k,v) -> System.out.println(k + " : " + v));
+        
+
+
+        Map<String, Double> maps2 = books.stream()
+                                         .collect( Collectors.toMap(     book -> book.getTitle(), book -> book.getPrice(),     (v1, v2) -> v1+v2));
+        maps2.forEach( (k,v) -> System.out.println(k + " : " + v));
+    }
+
+}
