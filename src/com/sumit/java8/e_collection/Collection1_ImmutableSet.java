490e87dba21ba96c57a3df0854d7fd712e5647ad
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-16 20:17:44 -0500

    Java 8 : Collection : Immutable Set

---
+++ src/com/sumit/java8/e_collection/Collection1_ImmutableSet.java
@@ -0,0 +1,24 @@
+package com.sumit.java8.e_collection;
+
+import java.util.*;
+
+public class Collection1_ImmutableSet {
+
+    // Different ways to create a Immutable set
+
+    public static void main(String[] args) {
+        Set<String> tempSet = new HashSet<>();
+        tempSet.add("G");
+        tempSet.add("H");
+        tempSet.add("I");
+
+
+        // Way-1
+        Set<String> immutable_set2 = Set.of("D", "E", "F");
+
+        // Way-2
+        Set<String> immutable_set3 = Set.copyOf(tempSet);
+
+    }
+
+}
