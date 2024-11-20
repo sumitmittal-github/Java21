ca3b599dcc08d7f01f3102223ee7279d218a4109
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-20 17:32:01 -0500

    Java 8 : IO : Console class

---
+++ src/com/sumit/java8/k_io/IO3_Console.java
@@ -0,0 +1,21 @@
+package com.sumit.java8.k_io;
+
+
+import java.io.Console;
+
+public class IO3_Console {
+
+    public static void main(String[] args) throws Exception {
+
+        Console console = System.console();
+        if(console == null)
+            System.out.println("Console not available. Your IDE has the console instance");
+        else {
+            //
+            //
+            //
+            
+        }
+    }
+
+}
