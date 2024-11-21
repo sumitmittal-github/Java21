a830aa38a5268f5ccf880ce583028e211bce1272
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-11-21 13:45:30 -0500

    Java 11 : var - Local Variable Type Inference

---
+++ src/com/sumit/java11/a_var/Example.java
@@ -0,0 +1,16 @@
+package com.sumit.java11.a_var;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        // initialize when declaring
+        var firstName = "sumit";
+
+        // cast the null to a type first
+        var lastName = (String) null;
+
+
+    }
+
+}
