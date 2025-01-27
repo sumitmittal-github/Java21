7249a922291dfffa685a69d6ae2e3db903c5bd42
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-01-26 19:32:40 -0500

    Java 17 : Records implementation

--- src/com/sumit/java17/b_records/RecordsExample.java
+++ src/com/sumit/java17/b_records/RecordsExample.java
@@ -2,8 +2,24 @@ package com.sumit.java17.b_records;
 
 public record RecordsExample(String name, int age) {
 
-    
+    // custom canonical constructor
+    /*private RecordsExample(String name, int age){
+        if(age<18)
+            throw new IllegalArgumentException("Minimum age should be 18 !!");
+        this.name = name;
+        this.age = age;
+    }*/
 
+    // compact constructor
+    public RecordsExample{
+        if(age<18)
+            throw new IllegalArgumentException("Minimum age should be 18 !!");
+    }
 
+    // override getter
+    @Override
+    public String name(){
+        return name.toUpperCase();
+    }
 
 }
