98c08aa63dd2d1c68534b494b8a06df9cb928d14
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-01-30 18:09:18 -0500

    Java 21 : Instance Main Method

---
+++ src/com/sumit/java21/a_instance_main_method/IMM_Example.java
@@ -0,0 +1,22 @@
+
+public class IMM_Example {
+
+    /*public static void main(String[] args) {
+        System.out.println("Traditional Main Method");
+    }*/
+
+    void main(){
+        System.out.println("Instance Main Method");
+        staticMethod();
+        instanceMethod();
+    }
+
+    static void staticMethod(){
+        System.out.println("static method");
+    }
+
+    void instanceMethod(){
+        System.out.println("instance method");
+    }
+
+}
